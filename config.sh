4908d0b8594276009b572a72110d03b61d2593e9
Jarl-Penguin <jarlpenguin@outlook.com>
2021-02-18 17:05:14 +0300

    Cedric
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export ARCH="arm64"
-export device="montana"
+export device="cedric"
 export device_is_ab="false"
 
 export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
 export branch="lineage-17.1"
-export name="LineageOS Kernel for montana"
-export defconfig="montana_defconfig"
+export name="LineageOS Kernel for cedric"
+export defconfig="cedric_defconfig"
 export clang="false"
 export clang_version=""
