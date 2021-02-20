6c927306e82ff7022a4d02b5fca676772372ffb9
Jarl-Penguin <jarlpenguin@outlook.com>
2021-02-20 09:00:32 +0300

    staging
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export device="montana"
 export device_is_ab="false"
 
 export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
-export branch="lineage-17.1"
+export branch="staging/lineage-17.1"
 export name="LineageOS Kernel for montana"
 export defconfig="montana_defconfig"
 export clang="false"
