59759a85913bea16ec0dd11fcee6e4fb699b93ce
Jarl-Penguin <jarlpenguin@outlook.com>
2021-03-07 21:20:46 +0300

    jeter
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export ARCH="arm64"
-export device="montana"
+export device="jeter"
 export device_is_ab="false"
 
 export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
 export branch="staging/lineage-17.1"
-export name="LineageOS Kernel for montana"
-export defconfig="montana_defconfig"
+export name="LineageOS Kernel for jeter"
+export defconfig="jeter_defconfig"
 export clang="false"
 export clang_version=""
