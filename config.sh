f261bbd48cd29af85fe934d19d8da69250ce7d93
Jarl-Penguin <36794857+JarlPenguin@users.noreply.github.com>
2021-06-15 08:18:13 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export device="jeter"
 export device_is_ab="false"
 
 export kernel="https://github.com/JarlPenguin/android_kernel_motorola_msm8953"
-export branch="staging/lineage-17.1"
+export branch="lineage-18.1"
 export name="LineageOS Kernel for jeter"
 export defconfig="jeter_defconfig"
 export clang="false"
